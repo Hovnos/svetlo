bdd1617fce32fc61add1264256489b025adc410b
Hovnos <filipsimon5337@gmail.com>
2024-05-27 16:36:17 +0200

    Track dist directory

--- dist/svetlo/server/main.server.mjs
+++ dist/svetlo/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7D3XBVA4.mjs";import"./chunk-6MRC4VXO.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-GUHXJNDE.mjs";import"./chunk-6MRC4VXO.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
