243fce4efb402a3a6eb8ede473171edced17acaf
Hovnos <filipsimon5337@gmail.com>
2024-07-21 15:55:23 +0200

    rem mobilecss

--- dist/svetlo/server/main.server.mjs
+++ dist/svetlo/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XC3Y4W7M.mjs";import"./chunk-Q6CASVDH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-W54ZIOD3.mjs";import"./chunk-Q6CASVDH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
