3ff128b327335e61da80ec5a6cc178615480a13f
Hovnos <filipsimon5337@gmail.com>
2024-07-23 11:44:43 +0200

    partneri

--- dist/svetlo/server/main.server.mjs
+++ dist/svetlo/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LNRNGNIW.mjs";import"./chunk-Q6CASVDH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HQKKLMRP.mjs";import"./chunk-Q6CASVDH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
 import { Page4Component } from './page4/page4.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from "./footer/footer.component";
 
 
 
@@ -14,7 +15,7 @@ declare var $: any;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CommonModule, Page2Component, Page3Component, Page4Component, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, CommonModule, Page2Component, Page3Component, Page4Component, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   encapsulation: ViewEncapsulation.None
